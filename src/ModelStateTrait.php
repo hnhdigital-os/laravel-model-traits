55dc139785be428ce9f3809f586f1e84b19d0c91
Rocco Howard <rocco@bluora.com.au>
2016-06-16 02:05:58 +1000

    Added a trait for active, archived, and deleted states

---
+++ src/ModelStateTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bluora\LarvelModelTraits;
+
+trait ModelStateTrait
+{
+
+    /**
+     * Scope a query to only include active models.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query, $type = true)
+    {
+        return $query->archived(false)->deleted(false);
+    }
+
+    /**
+     * Scope a query to only include archived models.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeArchived($query, $type = true)
+    {
+        if ($type === true) {
+            return $query->whereNotNull('archived_at');
+        }
+        return $query->whereNull('archived_at');
+    }
+
+    /**
+     * Scope a query to only include deleted models.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDeleted($query, $type = true)
+    {
+        if ($type === true) {
+            return $query->whereNotNull('deleted_at');
+        }
+        return $query->whereNull('deleted_at');
+    }
+}
